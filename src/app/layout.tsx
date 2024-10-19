6fc07107c4a89a4fe153e5856dec35e2d2b615c3
Cool <990299103@qq.com>
2024-10-19 23:56:00 +0800

    feat:"项目初始化，Redux持久化配置"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,52 @@
-import type { Metadata } from "next";
+"use client";
+// import type { Metadata } from "next";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "./globals.css";
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import BasicLayout from "@/layouts/BasicLayout";
+import { Provider, useDispatch } from "react-redux";
+import { getLoginUserUsingGet } from "@/api/userController";
+import { setLoginUser } from "@/stores/loginUser";
+import store, { AppDispatch } from "@/stores";
 
-export const metadata: Metadata = {
-  title: "编程潘多拉",
-  description: "Code by Cool",
+// export const metadata: Metadata = {
+//   title: "编程潘多拉",
+//   description: "Code by Cool",
+// };
+
+/**
+ * 初始化布局（多封装一层，使得能调用 useDispatch）
+ * @param children
+ * @constructor
+ */
+const InitLayout: React.FC<
+  Readonly<{
+    children: React.ReactNode;
+  }>
+> = ({ children }) => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  // 初始化全局用户状态
+  const doInitLoginUser = useCallback(async () => {
+    // 获取用户信息
+    const res = await getLoginUserUsingGet();
+    if (res.data) {
+      dispatch(setLoginUser(res.data));
+    } else {
+      // todo 测试代码，实际可删除
+      setTimeout(() => {
+        const testUser = { userName: "测试登录", id: 1 };
+        dispatch(setLoginUser(testUser));
+      }, 3000);
+    }
+
+  }, []);
+
+  useEffect(() => {
+    doInitLoginUser();
+  }, []);
+
+  return <>{children}</>;
 };
 
 export default function RootLayout({
@@ -18,7 +58,11 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <AntdRegistry>
-          <BasicLayout>{children}</BasicLayout>
+          <Provider store={store}>
+            <InitLayout>
+              <BasicLayout>{children}</BasicLayout>
+            </InitLayout>
+          </Provider>
         </AntdRegistry>
       </body>
     </html>
--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -9,6 +9,8 @@ import Link from "next/link";
 import GlobalFooter from "@/components/GlobalFooter";
 import { menus } from "../../../config/menu";
 import "./index.css";
+import { RootState } from "@/stores";
+import { useSelector } from "react-redux";
 
 /**
  * 搜索条
@@ -54,6 +56,7 @@ interface Props {
 export default function BasicLayout({ children }: Props) {
   const pathname = usePathname();
 
+  const loginUser = useSelector((state: RootState) => state.loginUser);
   return (
     <div
       id="basicLayout"
@@ -77,9 +80,9 @@ export default function BasicLayout({ children }: Props) {
           pathname,
         }}
         avatarProps={{
-          src: "https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg",
+          src: loginUser.userAvatar || "/assets/logo.png",
           size: "small",
-          title: "Cool",
+          title: loginUser.userName || "没有账号",
           render: (props, dom) => {
             return (
               <Dropdown
